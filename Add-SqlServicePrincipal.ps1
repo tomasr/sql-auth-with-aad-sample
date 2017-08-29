cbadaa9744312acfa4542e9fd5ddca71ad5ac134
Tomas Restrepo <tomas@winterdom.com>
2017-08-29 18:57:02 -0500

    Add script to grant permissions in SQL to the Service Principal

--- Add-SqlServicePrincipal.ps1
+++ Add-SqlServicePrincipal.ps1
@@ -10,7 +10,7 @@ param(
     [String]$servicePrincipalName
 )
 
-# For this script to work we need SqlCmd v13.1 or later
+# For this script to work we need SqlCmd v14.1 or later
 # which can be downloaded from
 # https://www.microsoft.com/en-us/download/details.aspx?id=53591
 # and the latest ODBC driver from
